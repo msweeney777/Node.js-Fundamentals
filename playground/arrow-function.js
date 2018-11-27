d3a473762b591d57a77f2129e86947c005b55ac7
Matt Sweeney <msweeney777@gmail.com>
2018-11-26 19:01:35 -0500

    First commit

---
+++ playground/arrow-function.js
@@ -0,0 +1,16 @@
+const square = x => x * x;
+console.log(square(9));
+
+var user = {
+  name: 'Matt',
+  sayHi: () => {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  },
+  sayHiAlt () {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  }
+};
+
+user.sayHiAlt(1, 2, 3);
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+  name: "Dr. Jekyll"
+};
+
+person.age = 39;
+
+debugger;
+
+person.name = 'Mike';
+
+console.log(person);
